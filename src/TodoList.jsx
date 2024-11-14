c231217de4a1b7f29acf3bd1975e19ffd8ff2340
Mariano <marianolumbreras@gmail.com>
2024-11-14 14:07:55 -0300

    todolist

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import TodoItem from './TodoItem';
 
-const TodoList = ({ todos, onToggle, onRemove }) => {
+const TodoList = ({ todos = [], onToggle, onRemove }) => {
   return (
     <ul className="w-full max-w-lg space-y-4">
-      {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} onToggle={onToggle} onRemove={onRemove} />
-      ))}
+      {Array.isArray(todos) && todos.length > 0 ? (
+        todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} onToggle={onToggle} onRemove={onRemove} />
+        ))
+      ) : (
+        <p>No hay tareas para mostrar.</p>
+      )}
     </ul>
   );
 };
 
 export default TodoList;
+
 
 
 
